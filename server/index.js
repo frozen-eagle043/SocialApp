8d6c9655525c95cc9b593fe716bfbd719eed6033
Shivam Devaser <55057194+frozen-eagle043@users.noreply.github.com>
2023-09-09 19:32:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,12 +29,20 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-const corsOptions = {
-  origin: 'https://social-app-eta-eight.vercel.app',
-  // You can add more allowed origins here if needed.
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOpts));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
